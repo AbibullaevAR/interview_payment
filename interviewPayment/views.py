bb6c2030d510c124c3a0e154b8a80220497ed98b
AbibullaevAR <a.alim2019@mail.ru>
2022-11-25 20:09:08 +0300

    add view for list items

--- interviewPayment/views.py
+++ interviewPayment/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+import stripe
+from django.shortcuts import render, redirect
+from django.views.generic import ListView
 
+from .models import Item
 # Create your views here.
+
+
+class ListItems(ListView):
+    queryset = Item.objects.all()
+    template_name = 'interviewPayment/list_items.html'
+    context_object_name = 'items'
+

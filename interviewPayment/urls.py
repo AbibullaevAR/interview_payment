b2423fba1d330ca8f559422fbb5df3f17160115b
AbibullaevAR <a.alim2019@mail.ru>
2022-11-26 01:38:43 +0300

    add api urls

--- interviewPayment/urls.py
+++ interviewPayment/urls.py
@@ -1,8 +1,11 @@
-from django.urls import path
+from django.urls import path, include
 
 from .views import ListItems
+
 app_name = 'interviewPayment'
 
 urlpatterns = [
-    path('items/', ListItems.as_view(), name='list_items')
+    path('items/', ListItems.as_view(), name='list_items'),
+
+    path('api/', include('interviewPayment.api.urls'))
 ]

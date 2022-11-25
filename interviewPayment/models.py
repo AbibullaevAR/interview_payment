c4c9081a8a2267d60f87db78f5e7340e7f61fe0f
AbibullaevAR <a.alim2019@mail.ru>
2022-11-25 20:10:58 +0300

    create models item, order

--- interviewPayment/models.py
+++ interviewPayment/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Item(models.Model):
+
+    name = models.CharField(max_length=200, unique=True)
+
+    description = models.TextField()
+
+    price = models.FloatField()
+
+    currency = models.CharField(max_length=3)
+
+
+class Order(models.Model):
+
+    items = models.ManyToManyField(Item)
+

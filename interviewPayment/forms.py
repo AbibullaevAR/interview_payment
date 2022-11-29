b288154538b419f029da2e2d1f8ee45c8a36fa5f
AbibullaevAR <a.alim2019@mail.ru>
2022-11-29 15:40:16 +0300

    fix: request to db on makemigrations command

--- interviewPayment/forms.py
+++ interviewPayment/forms.py
@@ -8,6 +8,6 @@ class OrderPaymentForm (forms.Form):
     discount = forms.BooleanField(required=False)
 
     currency = forms.ChoiceField(
-        choices=[(currency.currency_code, currency.currency_code) for currency in Currency.objects.all()],
+        choices=lambda: [(currency.currency_code, currency.currency_code) for currency in Currency.objects.all()],
         required=True
     )

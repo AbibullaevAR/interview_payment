a2373980ec4569dd6bc718f0879f7cc8b7b5e039
AbibullaevAR <a.alim2019@mail.ru>
2022-11-30 18:33:59 +0300

    add: register models

--- interviewPayment/admin.py
+++ interviewPayment/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+
+from .models import Currency, Item, Discount, Order
 # Register your models here.
+admin.site.register(Currency)
+admin.site.register(Item)
+admin.site.register(Discount)
+admin.site.register(Order)
